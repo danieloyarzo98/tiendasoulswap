3945b5059bdebbde0c6ba364611758b4f960ce1b
danielch1998 <doyarzo48@gmail.com>
2023-07-01 19:55:20 -0400

    se agrega elephantsql

---
+++ Backend/elephantsql.js
@@ -0,0 +1,6 @@
+const dbConfig = {
+    connectionString: 'postgres://nopacxbn:9Ttydq8moocuXXBAm-fBRKaVIGStgMzt@silly.db.elephantsql.com/nopacxbn   ',
+    username: 'nopacxbn',
+    password: '9Ttydq8moocuXXBAm-fBRKaVIGStgMzt',
+  };
+  
